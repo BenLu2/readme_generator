90898a348f7a94f9ab793401bbcc0035751fc907
BenLu2 <90216349+BenLu2@users.noreply.github.com>
2021-11-13 23:43:48 -0800

    new

--- index.js
+++ index.js
@@ -5,6 +5,34 @@ const fs = require('fs');
 //prompt question
 inquirer
     .prompt([
+
+        {
+            type: 'input',
+            message: " What is the title?",
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: " Description of your github?",
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: " What is the user story?",
+            name: 'userstory'
+        },
+        {
+            type: 'input',
+            message: " How to install?",
+            name: 'install'
+        },
+
+        {
+            type: 'input',
+            message: " How to get support?",
+            name: 'support'
+        },
+
         {
             type: 'input',
             message: " What is your github account user name?",
@@ -12,18 +40,73 @@ inquirer
         },
         {
             type: 'input',
-            message: " What is your user email?",
+            message: " What is your email?",
             name: 'email'
         },
         {
             type: 'input',
-            message: " What is your user name?",
-            name: 'username'
-        },
-        {
-            type: 'input',
-            message: " What is your user name?",
-            name: 'username'
+            message: " What is your user Linkedin?",
+            name: 'Linkedin'
         },
 
-    ])
+    ]).then(
+        (
+            {
+                title,
+                description,
+                userstory,
+                install,
+                support,
+                username,
+                email,
+                Linkedin,
+
+
+                
+            }
+        ) =>
+        {
+            const template = `# ${title}
+            
+            *[description](#description)
+            *[userstory](#userstory)
+            *[install](#install)
+            *[support](#support)
+            
+            #Description
+            ${description}
+
+            ##User Story
+            ${userstory}
+
+            ##Install
+            ${install}
+
+            ##Get Support
+            ${support}
+
+            #Contact Me
+            *Github :${username}
+            *E-mail :${email}
+            *Linkedin :${Linkedin}
+            `;
+
+            //Create the new readMe by using the fs
+            createNewFile(title, template);
+        }
+    );
+        function createNewFile(fileName, data)
+        {
+            fs.writeFile(`./s{fileName.toLowerCase().split(' ').join(' ')}.md`, data, (error) => {
+                if(error)
+                {
+                    console.log(error)
+                }
+                else
+                {
+                    console.log('New readMe file has successfully generated!');
+                }
+            })
+            
+        }
+    
